83607170521b833287ddfff14e2996122f4bf1bf
Adrien Thebo <git@somethingsinistral.net>
2013-07-05 17:22:42 -0700

    Don't use an iterator in the network pool
    For whatever reason, using a persistent iterator in an AutoNetwork::Pool
    within the official Vagrant 1.2.2 ruby on OSX Mountain Lion causes
    FiberErrors, as follows:
    
        /Users/zalgo/src/vagrants/vagrant-auto_network/lib/auto_network/pool.rb:22:in `next': fiber called across threads (FiberError)
            from /Users/zalgo/src/vagrants/vagrant-auto_network/lib/auto_network/pool.rb:22:in `next'
            from /Users/zalgo/src/vagrants/vagrant-auto_network/lib/auto_network/mixin.rb:11:in `auto_network!'
            from /Users/zalgo/src/vagrants/hiera_mysql/Vagrantfile:48:in `block (2 levels) in <top (required)>'
        [stack trace continues]
    
    It's unclear what is triggering this, but it looks to be an ugly
    intersection of how iterators handle state intersecting with some
    internal threading. This commit removes the iterator and instead mutates
    an array of available addresses as they are fetched.

--- lib/auto_network/pool.rb
+++ lib/auto_network/pool.rb
@@ -5,21 +5,35 @@ require 'ipaddress'
 module AutoNetwork
 class Pool
 
-  # @todo remove hack.
+  class PoolExhaustedError < StandardError; end
+
+  # Provide a single instance of this class
+  #
+  # @return [AutoNetwork::Pool]
   def self.instance
     @myself ||= new
   end
 
-  def initialize(range = AutoNetwork.default_pool)
-    @network  = IPAddress.parse(range)
-    @iterator = @network.hosts.each
+  # @param addr [String] The network address range to use as the address pool.
+  #   Defaults to `AutoNetwork.default_pool`
+  def initialize(addr = AutoNetwork.default_pool)
+    @addr    = addr
+    @network = IPAddress.parse(addr)
 
-    # Assume that the first valid host address is used by the host address.
-    @iterator.next
+    @range = @network.hosts
+    # Drop the first IP address as it should be reserved for the host system
+    @range.shift
   end
 
+  # @return [String] The string representation of the next available address
+  # @raise [PoolExhaustedError] There are no remaining addresses in the pool.
   def next
-    @iterator.next.address
+    if @range.empty?
+      raise PoolExhaustedError, "No addresses remaining in network address pool #{addr.inspect}"
+    end
+
+    addr = @range.shift
+    addr.address
   end
 end
 end
