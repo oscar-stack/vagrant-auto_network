7f556e9605048155646ea98c13142844fb37bfe9
Adrien Thebo <git@somethingsinistral.net>
2013-08-02 16:20:05 -0700

    Randomly change auto_network/plugin indentation

--- lib/auto_network/plugin.rb
+++ lib/auto_network/plugin.rb
@@ -1,21 +1,23 @@
 require 'vagrant'
 require 'auto_network/action'
 
-class AutoNetwork::Plugin < Vagrant.plugin('2')
-  name 'auto_network'
+module AutoNetwork
+  class Plugin < Vagrant.plugin('2')
+    name 'auto_network'
 
-  description <<-DESC
-  This plugin adds support for automatically configuring Vagrant hostonly
-  networks.
-  DESC
+    description <<-DESC
+    This plugin adds support for automatically configuring Vagrant hostonly
+    networks.
+    DESC
 
-  %w[up reload].each do |action_type|
-    action = "machine_action_#{action_type}".to_sym
-    action_hook(:auto_network, action) do |hook|
-      hook.before(
-        VagrantPlugins::ProviderVirtualBox::Action::Network,
-        AutoNetwork::Action::PersistentIP
-      )
+    %w[up reload].each do |action_type|
+      action = "machine_action_#{action_type}".to_sym
+      action_hook(:auto_network, action) do |hook|
+        hook.before(
+          VagrantPlugins::ProviderVirtualBox::Action::Network,
+          AutoNetwork::Action::PersistentIP
+        )
+      end
     end
   end
 end
