c9ffbc52bffe278d2b57d7ff8eee66606c9c8c2a
Adrien Thebo <git@somethingsinistral.net>
2013-08-05 18:17:26 -0700

    Handle (de)serialization in action class

--- lib/auto_network/action/gen_pool.rb
+++ lib/auto_network/action/gen_pool.rb
@@ -7,9 +7,33 @@ class AutoNetwork::Action::GenPool
   def call(env)
     @env = env
 
-    @env[:auto_network_pool] = AutoNetwork::Pool.new
-
+    deserialize!
     @app.call(@env)
+    serialize!
   end
 
+  private
+
+  def deserialize!
+    pool = nil
+    if statefile.exist?
+      yaml = statefile.read
+      pool = YAML.load(statefile.read)
+    else
+      pool = AutoNetwork::Pool.new
+    end
+    @env[:auto_network_pool] = pool
+  end
+
+  def serialize!
+    data = YAML.dump(@env[:auto_network_pool])
+
+    statefile.open('w') do |fh|
+      fh.write(data)
+    end
+  end
+
+  def statefile
+    @statefile ||= @env[:home_path].join('auto_network.yaml')
+  end
 end
