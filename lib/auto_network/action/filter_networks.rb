8fd28777b20f2c5f8d524dee6335f4485cb6c868
Adrien Thebo <git@somethingsinistral.net>
2013-08-07 16:48:58 -0700

    Refactor action/filter_networks for clarity

--- lib/auto_network/action/filter_networks.rb
+++ lib/auto_network/action/filter_networks.rb
@@ -8,29 +8,39 @@ class AutoNetwork::Action::FilterNetworks
     @app, @env = app, env
   end
 
+
+  # Convert auto_network interfaces to static private_network interfaces.
+  #
+  # @param env [Hash]
+  #
+  # @option env [AutoNetwork::Pool] auto_network_pool The global auto network pool
+  # @option env [Vagrant::Environment] env The Vagrant environment containing
+  #   the active machines that need to be filtered.
+  #
+  # @return [void]
   def call(env)
     @env = env
 
-    @pool    = @env[:auto_network_pool]
-
+    @pool      = @env[:auto_network_pool]
     global_env = @env[:env]
 
-    if global_env
-      active_machines = global_env.active_machines.map do |vm_id|
-        global_env.machine(*vm_id)
-      end
 
-      active_machines.each do |m|
-        @machine = m
-        assign_address if machine_has_address?
-      end
-      @machine = nil
+    machines = machines_for_env(global_env)
+
+    active_machines.each do |m|
+      @machine = m
+      assign_address if machine_has_address?
     end
+    @machine = nil
 
     @app.call(@env)
   end
 
   private
+
+  def machines_for_env(global_env)
+    global_env.active_machines.map { |vm_id| global_env.machine(*vm_id) }
+  end
 
   def machine_has_address?
     @machine and @pool.address_for(@machine)
