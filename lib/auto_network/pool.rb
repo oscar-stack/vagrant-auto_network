ce29050cf576b778d08593f3a2288e5306d4025a
Charlie Sharpsteen <source@sharpsteen.net>
2014-04-22 21:58:36 -0700

    Update documentation for auto_network/pool
    Completed doc coverage of all public methods along with small tweaks and
    fixups.

--- lib/auto_network/pool.rb
+++ lib/auto_network/pool.rb
@@ -1,23 +1,33 @@
 require 'ipaddr'
 
 module AutoNetwork
+  # The Pool is a class that manages a range of IP addresses and manages the
+  # allocation of specific addresses to individual Vagrant machines.
   class Pool
 
+    # An error class raised when no allocatable addresses remain in the Pool.
+    #
+    # @api private
     class PoolExhaustedError < Vagrant::Errors::VagrantError
       error_key(:pool_exhausted, 'vagrant_auto_network')
     end
 
-    # @param network_addr [String] The network address range to use as the
+    # Create a new Pool object that manages a range of IP addresses.
+    #
+    # @param network_range [String] The network address range to use as the
     #   address pool.
     def initialize(network_range)
       @network_range = network_range
       generate_pool
     end
 
-    # Retrieve an IP address for the given machine. If a machine already has
-    # an IP address requested, then return that.
+    # Allocate an IP address for the given machine. If a machine already has an
+    # IP address allocated, then return that.
     #
     # @param machine [Vagrant::Machine]
+    # @return [IPAddr] the IP address assigned to the machine.
+    # @raise [PoolExhaustedError] if no allocatable addresses remain in the
+    #   range managed by the pool.
     def request(machine)
       if (address = address_for(machine))
         return address
@@ -31,15 +41,21 @@ module AutoNetwork
       end
     end
 
-    # Release an IP address associated with a machine
+    # Release an IP address associated with a machine.
     #
     # @param machine [Vagrant::Machine]
+    # @return [nil]
     def release(machine)
       if (address = address_for(machine))
         @pool[address] = nil
       end
     end
 
+    # Look up the address assigned to a given machine.
+    #
+    # @param machine [Vagrant::Machine]
+    # @return [IPAddr] the IP address assigned to the machine.
+    # @return [nil] if the machine has no address assigned.
     def address_for(machine)
       return nil if machine.id.nil?
       next_addr, _ = @pool.find { |(addr, id)| machine.id == id }
