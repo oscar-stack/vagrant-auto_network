b0e7449e2f513f674ef9610b965dbf95fb6ac44c
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-03 12:29:34 -0700

    Sync pool operations and eliminate race conditions
    This patch re-implements Pool storage using `YAML::Store` from the Ruby stdlib.
    `YAML::Store` provides a hash with autoflushing file-backed storage. Concurrent
    operations between multiple processes are protected by `File.flock` (to the
    extent that `flock` works on a given filesystem).
    
    Previously, persistance was handled by de-serializing a `Pool` or `PoolManager`
    object from storage at the beginning of a Vagrant run and then re-searializing
    it when the run completes. This method enabled some notable race conditions:
    
      - If the Vagrant process did not exit cleanly, an updated pool file would
        never be written. However, the vagrant process may started VMs that are now
        using IP addresses from the pool.
    
      - Starting multiple Vagrant processes would result in divergent states that
        could not be reconciled.

--- lib/auto_network/pool_manager.rb
+++ lib/auto_network/pool_manager.rb
@@ -1,54 +1,64 @@
 require 'auto_network/pool'
+require 'yaml/store'
 require 'ipaddr'
 
 # Manages a collection of IP address Pools. One per provider.
 module AutoNetwork
   class PoolManager
-    def initialize(pools = {})
-      @pools = pools
+    def initialize(path)
+      @pool_storage = YAML::Store.new(path)
+    end
+
+    # Locates or creates a pool for the given machine and yields the pool to a
+    # block. This is all done inside of a transaction so that the pool may be
+    # safely modified.
+    def with_pool_for(machine, read_only=false)
+      @pool_storage.transaction(read_only) do
+        pool = lookup_pool_for(machine)
+        pool ||= generate_pool_for(machine)
+
+        yield pool
+      end
     end
 
     # The `request`, `release` and `address_for` methods are all proxied
     # straight through to the underlying Pool objecs.
     def request(machine)
-      if (pool = pool_for(machine))
-        return pool.request(machine)
-      else
-        generate_pool_for(machine).request(machine)
+      with_pool_for(machine) do |pool|
+        pool.request(machine)
       end
     end
 
     def release(machine)
-      if (pool = pool_for(machine))
+      with_pool_for(machine) do |pool|
         pool.release(machine)
-      else
-        nil
       end
     end
 
     def address_for(machine)
-      if (pool = pool_for(machine))
-        return pool.address_for(machine)
-      else
-        return nil
+      with_pool_for(machine, read_only=true) do |pool|
+        pool.address_for(machine)
       end
     end
 
     private
 
-    def pool_for(machine)
-      @pools[machine.provider_name.to_s]
+    # NOTE: Both lookup_pool_for and generate_pool_for *must* be called from
+    # within a transaction.
+
+    def lookup_pool_for(machine)
+      @pool_storage['pools'][machine.provider_name.to_s]
     end
 
     def generate_pool_for(machine)
-      @pools[machine.provider_name.to_s] = AutoNetwork::Pool.new(next_pool_range)
-      @pools[machine.provider_name.to_s]
+      @pool_storage['pools'][machine.provider_name.to_s] = AutoNetwork::Pool.new(next_pool_range)
+      @pool_storage['pools'][machine.provider_name.to_s]
     end
 
     # A bit hacky. Assumes all Pools use a "/24" address range.
     def next_pool_range
       # Look up the highest "XX.XX.YY.XX/24" range in use.
-      last_pool_range = IPAddr.new(@pools.values.map{|v| v.network_range}.sort.last)
+      last_pool_range = IPAddr.new(@pool_storage['pools'].values.map{|v| v.network_range}.sort.last)
       # Increment "YY" by one to generate a new "/24" range.
       new_pool_range = ((last_pool_range >> 8).succ << 8)
 
