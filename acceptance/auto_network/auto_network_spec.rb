d24c63f99149023ae05726c38b5984911efffb1d
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-06 11:51:01 -0700

    Extend acceptance testing to status and reload
    Ensure that the `status` and `reload` commands continue to work when
    auto_network is loaded. Vagrant 1.6.0 introduced some internal changes that
    broke `reload`.
    
    Also partially reverts cddc317 by adding back a `vagrant destroy` run in an
    `after` block. This is redundant, but ensures everything is cleand up if the
    acceptance test fails part of the way through.

--- acceptance/auto_network/auto_network_spec.rb
+++ acceptance/auto_network/auto_network_spec.rb
@@ -11,11 +11,22 @@ shared_examples 'provider/auto_network' do |provider, options|
     assert_execute('vagrant', 'box', 'add', 'box', options[:box])
   end
 
+  after do
+    # Ensure any VMs that survived tests are cleaned up.
+    execute('vagrant', 'destroy', '--force', log: false)
+  end
+
+  # NOTE: This is a bit tightly coupled as it strings together tests for
+  # vagrant up, status, reload and destroy in a single case. However, each
+  # invocation is dependant on the state created by the prior command.
   it 'manages IP address allocation for the lifecycle of a VM' do
     result = assert_execute('vagrant', 'up', "--provider=#{provider}")
     expect(result.stdout).to match(/Assigning "10\.42\.1\.2" to 'default'/)
 
-    result = assert_execute('vagrant', 'destroy', '--force', log: false)
+    assert_execute('vagrant', 'status')
+    assert_execute('vagrant', 'reload', 'default')
+
+    result = assert_execute('vagrant', 'destroy', '--force')
     expect(result.stdout).to match(/Releasing "10\.42\.1\.2" from default/)
   end
 end
