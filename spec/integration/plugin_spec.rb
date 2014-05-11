64647b86c1ad024e84f8e8df54751a6be3653cb1
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-11 09:44:44 -0700

    Add integration test for filter_networks
    Most of the changes in this patch involve creating `vagrant-spec` fixtures that
    can support complicated integration tests.
    
    All AutoNetwork actions that fire during envrionment initialization are now
    covered by integration tests.

--- spec/integration/plugin_spec.rb
+++ spec/integration/plugin_spec.rb
@@ -3,26 +3,82 @@ require 'spec_helper'
 describe AutoNetwork::Plugin do
   include_context 'vagrant-unit'
 
-  context 'when a vagrant environment is initialized' do
-    let(:settings) {
-      Module.new do
-        extend AutoNetwork::Settings
-      end
-    }
-    let(:manager_double) { double().as_null_object }
+  # Stub the settings attached to the AutoNetwork module so we don't change
+  # global state during tests.
+  let(:settings) {
+    Module.new do
+      extend AutoNetwork::Settings
+    end
+  }
 
-    it 'sets the global pool_manager' do
-      allow(AutoNetwork).to receive(:pool_manager=) do |pool_manager|
-        settings.pool_manager = pool_manager
-      end
-      allow(manager_double).to receive(:new).and_return('a_pool_manager')
-      stub_const('AutoNetwork::PoolManager', manager_double)
+  before(:each) do
+    allow(AutoNetwork).to receive(:pool_manager=) do |pool_manager|
+      settings.pool_manager = pool_manager
+    end
+    allow(AutoNetwork).to receive(:pool_manager).and_return { settings.pool_manager }
+  end
 
-      # Create an environment \o/
-      isolated_environment.create_vagrant_env
-
-      expect(settings.pool_manager).to eq('a_pool_manager')
+  # Configure each testing environment to contain an AutoNetwork pool file and
+  # a single existing machine that has been allocated in the pool
+  let(:test_env) { isolated_environment }
+  before(:each) do
+    pool_dir = test_env.homedir.join('auto_network')
+    pool_dir.mkpath
+    pool_dir.join('pool.yaml').open('w+') do |f|
+      f.write <<-EOF
+---
+poolfile_version: 2
+pools:
+  dummy: !ruby/object:AutoNetwork::Pool
+    network_range: 10.20.1.0/29
+    pool:
+      10.20.1.2:
+        path: #{test_env.workdir}
+        name: test1
+      10.20.1.3: 
+      10.20.1.4: 
+      10.20.1.5: 
+      10.20.1.6: 
+EOF
     end
 
+    test_env.vagrantfile <<-EOF
+Vagrant.configure("2") do |config|
+  config.vm.define 'test1' do |node|
+    node.vm.network :private_network, :auto_network => true
+  end
+end
+EOF
+
+    # Touch an ID file so that Vagrant thinks test1 exists. The 'dummy'
+    # component of the path actually tells Vagrant which provider is managing
+    # the machine. vagrant-spec defines a dummy provider for us.
+    machine_dir = test_env.workdir.join('.vagrant/machines/test1/dummy')
+    machine_dir.mkpath
+    machine_dir.join('id').open('w+') { |f| f.write('') }
+  end
+
+  # Dispose of the temporary directory used to run the test.
+  after(:each) do
+    test_env.close
+  end
+
+
+  context 'when a vagrant environment is initialized' do
+    it 'sets the global pool_manager' do
+      # Create an environment \o/
+      test_env.create_vagrant_env
+
+      expect(settings.pool_manager).to be_a(AutoNetwork::PoolManager)
+    end
+
+    it 'assigns autonetworked IP addresses to existing machines' do
+      env = test_env.create_vagrant_env
+
+      test_machine = env.machine(:test1, :dummy)
+      _, network_opts = test_machine.config.vm.networks.find {|n| n.first == :private_network}
+
+      expect(network_opts).to include(:ip => '10.20.1.2')
+    end
   end
 end
