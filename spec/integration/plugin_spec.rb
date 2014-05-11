f9022cc503231d3c030685b0d460bcaa6a2cd5fc
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-11 10:34:53 -0700

    Add integration tests for release action
    Test that destroying a machine results in an IP address being released from the
    pool.

--- spec/integration/plugin_spec.rb
+++ spec/integration/plugin_spec.rb
@@ -81,4 +81,21 @@ EOF
       expect(network_opts).to include(:ip => '10.20.1.2')
     end
   end
+
+  context 'when destroying a machine' do
+    def current_ip(machine)
+      settings.pool_manager.with_pool_for(machine) {|p| p.address_for(machine)}
+    end
+
+    it 'releases the allocated IP address' do
+      env = test_env.create_vagrant_env
+      test_machine = env.machine(:test1, :dummy)
+
+      expect(current_ip(test_machine)).to eq('10.20.1.2')
+
+      test_machine.action(:destroy)
+
+      expect(current_ip(test_machine)).to be_nil
+    end
+  end
 end
