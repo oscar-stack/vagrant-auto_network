405758dedba849b90efc45545d8df4929a516b19
Charlie Sharpsteen <source@sharpsteen.net>
2014-05-12 10:50:59 -0700

    Split integration setup out into a shared context

--- spec/integration/plugin_spec.rb
+++ spec/integration/plugin_spec.rb
@@ -1,68 +1,9 @@
 require 'spec_helper'
+require_relative 'shared/auto_network_1x_context'
 
 describe AutoNetwork::Plugin do
   include_context 'vagrant-unit'
-
-  # Stub the settings attached to the AutoNetwork module so we don't change
-  # global state during tests.
-  let(:settings) {
-    Module.new do
-      extend AutoNetwork::Settings
-    end
-  }
-
-  before(:each) do
-    allow(AutoNetwork).to receive(:pool_manager=) do |pool_manager|
-      settings.pool_manager = pool_manager
-    end
-    allow(AutoNetwork).to receive(:pool_manager).and_return { settings.pool_manager }
-  end
-
-  # Configure each testing environment to contain an AutoNetwork pool file and
-  # a single existing machine that has been allocated in the pool
-  let(:test_env) { isolated_environment }
-  before(:each) do
-    pool_dir = test_env.homedir.join('auto_network')
-    pool_dir.mkpath
-    pool_dir.join('pool.yaml').open('w+') do |f|
-      f.write <<-EOF
----
-poolfile_version: 2
-pools:
-  dummy: !ruby/object:AutoNetwork::Pool
-    network_range: 10.20.1.0/29
-    pool:
-      10.20.1.2:
-        path: #{test_env.workdir}
-        name: test1
-      10.20.1.3: 
-      10.20.1.4: 
-      10.20.1.5: 
-      10.20.1.6: 
-EOF
-    end
-
-    test_env.vagrantfile <<-EOF
-Vagrant.configure("2") do |config|
-  config.vm.define 'test1' do |node|
-    node.vm.network :private_network, :auto_network => true
-  end
-end
-EOF
-
-    # Touch an ID file so that Vagrant thinks test1 exists. The 'dummy'
-    # component of the path actually tells Vagrant which provider is managing
-    # the machine. vagrant-spec defines a dummy provider for us.
-    machine_dir = test_env.workdir.join('.vagrant/machines/test1/dummy')
-    machine_dir.mkpath
-    machine_dir.join('id').open('w+') { |f| f.write('') }
-  end
-
-  # Dispose of the temporary directory used to run the test.
-  after(:each) do
-    test_env.close
-  end
-
+  include_context 'auto_network 1.x'
 
   context 'when a vagrant environment is initialized' do
     it 'sets the global pool_manager' do
---
+++ spec/integration/shared/auto_network_1x_context.rb
@@ -0,0 +1,53 @@
+require_relative 'isolated_settings_context'
+
+# Creates an environment for each test containing a small pool of AutoNetwork
+# addresses and a single "created" VM.
+shared_context 'auto_network 1.x' do
+  include_context 'vagrant-unit'
+  include_context 'auto_network-settings'
+
+  # Configure each testing environment to contain an AutoNetwork version 2 pool
+  # file and a single existing machine that has been allocated in the pool
+  let(:test_env) { isolated_environment }
+  let(:pool_file) { test_env.homedir.join('auto_network', 'pool.yaml') }
+  before(:each) do
+    pool_file.dirname.mkpath
+    pool_file.open('w+') do |f|
+      f.write <<-EOF
+---
+poolfile_version: 2
+pools:
+  dummy: !ruby/object:AutoNetwork::Pool
+    network_range: 10.20.1.0/29
+    pool:
+      10.20.1.2:
+        path: #{test_env.workdir}
+        name: test1
+      10.20.1.3: 
+      10.20.1.4: 
+      10.20.1.5: 
+      10.20.1.6: 
+EOF
+    end
+
+    test_env.vagrantfile <<-EOF
+Vagrant.configure("2") do |config|
+  config.vm.define 'test1' do |node|
+    node.vm.network :private_network, :auto_network => true
+  end
+end
+EOF
+
+    # Touch an ID file so that Vagrant thinks test1 exists. The 'dummy'
+    # component of the path actually tells Vagrant which provider is managing
+    # the machine. vagrant-spec defines a dummy provider for us.
+    machine_dir = test_env.workdir.join('.vagrant/machines/test1/dummy')
+    machine_dir.mkpath
+    machine_dir.join('id').open('w+') { |f| f.write('') }
+  end
+
+  # Dispose of the temporary directory used to run the test.
+  after(:each) do
+    test_env.close
+  end
+end
